2de4d787cb726e714326c815da4dfe13156f46a0
Your Name <you@example.com>
2025-12-27 16:55:34 +0200

    added circdisc.cpp

--- 7η Σειρά/circdisc.cpp
+++ 7η Σειρά/circdisc.cpp
@@ -1,8 +1,15 @@
-#include <iosteam>
+#include <iostream>
 using namespace std;
 
 int main() {
-    
+    double X, Y;
+    cin >> X >> Y;
+
+    double distSquared = X * X + Y * Y;
+
+    if (distSquared < 1.0) cout << "inside\n";
+    else if (distSquared == 1.0) cout << "border\n";
+    else cout << "outside\n";
 
     return 0;
 }
