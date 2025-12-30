b7887075f5208a4c85e2aaf038ffeffc12665439
KOSTAS FR <kostas.fragos18@gmailcom>
2025-12-30 19:38:59 +0200

    edited one file

--- useful_programs/stack_array.cpp
+++ useful_programs/stack_array.cpp
@@ -5,13 +5,13 @@ class stack {
 public:  
     stack();
     bool empty();
-    void push(int x);
+    void push(char x); // int x
     int pop();
     
 private:
     static const int size = 100;
     // or: #define size (int)(100)
-    int data[size];
+    char data[size]; // int data[size];
     int top;
 };
 
@@ -23,7 +23,7 @@ bool stack::empty() {
     return top == 0;
 }
 
-void stack::push(int x) {
+void stack::push(char x) { // int x
     data[top++] = x;
 }
 
@@ -33,9 +33,13 @@ int stack::pop() {
 
 int main() {
     stack s;
-    for (int i = 1; i <= 10; i++)
-        s.push(i);
+    // for (int i = 1; i <= 10; i++)
+    //     s.push(i);
+    int c;
+    while ((c = getchar()) != EOF) {
+        s.push(c);
+    }
 
     while (!s.empty())
-        printf("%d\n", s.pop());
+        printf("%c\n", s.pop());
 }
